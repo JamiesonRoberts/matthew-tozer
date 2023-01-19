ad7daebc919f9684633aedf1daffa8f5f9a92fb1
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-19 09:04:26 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import PlausibleProvider from 'next-plausible'
+
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({Component, pageProps}) => {
+    return (
+        <PlausibleProvider
+            domain='matthewtozer.com'
+            trackOutboundLinks={true}
+        >
+            <Component {...pageProps} />
+        </PlausibleProvider>
+    )
 }
+
+export default App
