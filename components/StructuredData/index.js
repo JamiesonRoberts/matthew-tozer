65c30009d56dfc00bbae62087cb760f9d72c6798
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-20 11:23:35 -0500

    Syntax error

--- components/StructuredData/index.js
+++ components/StructuredData/index.js
@@ -64,7 +64,7 @@ export default function StructuredData() {
                                         "sameAs": "https://www.queensu.ca/"
                                     }
                                 }
-                            },
+                            }
                         ],
                         "affiliation": [
                             {
