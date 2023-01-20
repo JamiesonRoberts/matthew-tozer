6bbf3d73e839e03f1d7a0b8af24b6d43544f7321
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-20 00:49:11 -0500

    Working on porting over content and styles as well as flexbox refinement

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import PlausibleProvider from 'next-plausible'
+import { Abel, Marcellus } from '@next/font/google'
+
+const abel = Abel({ weight: ['400'], subsets: ['latin'] })
+const marcellus = Marcellus({ weight: ['400'], subsets: ['latin'] })
 
 import '@/styles/globals.css'
 
-const App = ({Component, pageProps}) => {
+const App = ({ Component, pageProps }) => {
     return (
-        <PlausibleProvider
-            domain='matthewtozer.com'
-            trackOutboundLinks={true}
-        >
+        <PlausibleProvider domain='matthewtozer.com' trackOutboundLinks={true}>
+            <style jsx global>{`
+                :root {
+                    --abel: ${abel.style.fontFamily};
+                    --marcellus: ${marcellus.style.fontFamily};
+                }
+            `}</style>
             <Component {...pageProps} />
         </PlausibleProvider>
     )
