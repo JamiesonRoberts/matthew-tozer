bbc8049e5fa4e905e2f1f1c6b8158b867601944f
Jamieson Roberts <hello@jamiesonroberts.com>
2023-01-28 13:35:38 -0500

    Removing ISR that is currently broken with next 13

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,21 @@ import Main from '@/components/Main'
 import Footer from '@/components/Footer'
 import StructuredData from '@/components/StructuredData'
 
-function Home({ locationData }) {
+export async function getStaticProps() {
+    const res = await fetch(
+        `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJDyCfdJYfLIgR8LRERkyP-H8&key=${process.env.PLACES_SERVER_SIDE_API_KEY}`
+    )
+
+    const { result } = await res.json()
+
+    return {
+        props: {
+            locationData: result,
+        },
+    }
+}
+
+export default function Home({ locationData }) {
     return (
         <>
             <Head>
@@ -64,23 +78,3 @@ function Home({ locationData }) {
         </>
     )
 }
-
-export async function getStaticProps() {
-    const res = await fetch(
-        `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJDyCfdJYfLIgR8LRERkyP-H8&key=${process.env.PLACES_SERVER_SIDE_API_KEY}`
-    )
-
-    const { result } = await res.json()
-
-    return {
-        props: {
-            locationData: result || {},
-        },
-        // Next.js will attempt to re-generate the page:
-        // - When a request comes in
-        // - At most once every 10 seconds
-        revalidate: 43200, // In seconds
-    }
-}
-
-export default Home
